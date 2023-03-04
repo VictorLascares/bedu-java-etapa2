628ec98796864a8690746784364c8674043077c7
Victor Lascares <vmlascares@gmail.com>
2023-03-04 17:36:49 -0600

    Implementando el plugin application para ejecutar aplicacion de java que suma los primeros 100 numeros

---
+++ s1/r2/src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String [] args) {
+        int suma = 0;
+        for (int i = 1; i <= 100; i++) {
+            suma += i;
+        }
+
+        System.out.println("La suma de los primeros 100 numeros es: " + suma);
+    }
+}
