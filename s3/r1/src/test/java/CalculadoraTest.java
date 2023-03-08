3eabf7f7a08b286065cffdd150f611fa0e2d6150
Victor Lascares <vmlascares@gmail.com>
2023-03-08 16:26:05 -0600

    Arreglando error de copiado y agregando nuevo test

--- s3/r1/src/test/java/CalculadoraTest.java
+++ s3/r1/src/test/java/CalculadoraTest.java
@@ -17,20 +17,26 @@ class CalculadoraTest {
     @DisplayName("Prueba resta")
     void resta() {
         int expected = 1;
-        assertEquals(expected, calculadora.suma(3,2));
+        assertEquals(expected, calculadora.resta(3,2));
     }
 
     @Test
     @DisplayName("Prueba multiplica")
     void multiplica() {
         int expected = 6;
-        assertEquals(expected, calculadora.suma(3,2));
+        assertEquals(expected, calculadora.multiplica(3,2));
+    }
+
+    @Test
+    @DisplayName("Prueba divide entre 0")
+    void divide() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> calculadora.divide(100, 0));
+        assertEquals("No es posible dividir un valor entre 0", exception.getMessage());
     }
 
     @Test
     @DisplayName("Prueba divide")
-    void divide() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> calculadora.divide(100, 0));
-        assertEquals("No es posible dividir un valor entre 0", exception.getMessage());
+    void divide2() {
+        assertEquals(10, calculadora.divide(100,10));
     }
 }
