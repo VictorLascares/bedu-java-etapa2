15d7fa95be37f44231f14a727ea28c827badded4
Victor Lascares <vmlascares@gmail.com>
2023-03-08 16:06:58 -0600

    Realizar una prueba unitaria que se encargue de verificar que al momento de validar una funcionalidad, se lanza la excepción esperada.

---
+++ s3/r1/src/test/java/CalculadoraTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+    private Calculadora calculadora = new Calculadora();
+
+    @Test
+    @DisplayName("Prueba suma")
+    void suma() {
+        int expected = 5;
+        assertEquals(expected, calculadora.suma(3,2));
+    }
+
+    @Test
+    @DisplayName("Prueba resta")
+    void resta() {
+        int expected = 1;
+        assertEquals(expected, calculadora.suma(3,2));
+    }
+
+    @Test
+    @DisplayName("Prueba multiplica")
+    void multiplica() {
+        int expected = 6;
+        assertEquals(expected, calculadora.suma(3,2));
+    }
+
+    @Test
+    @DisplayName("Prueba divide")
+    void divide() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> calculadora.divide(100, 0));
+        assertEquals("No es posible dividir un valor entre 0", exception.getMessage());
+    }
+}
